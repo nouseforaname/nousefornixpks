51438bd137dc13357e96081c3770f6f32199132e
nouseforaname <34882943+nouseforaname@users.noreply.github.com>
2025-02-10 17:52:56 +0100

    exclude some gnome defaults
    no need for them

--- configuration.nix
+++ configuration.nix
@@ -145,6 +145,24 @@ in
     interactiveShellInit = ''
       . ${config.users.users.nouseforaname.home}/workspace/nousefornixpks/bashrc
     '';
+    gnome.excludePackages = (with pkgs; [
+      atomix # puzzle game
+      cheese # webcam tool
+      nano
+      epiphany # web browser
+      evince # document viewer
+      geary # email reader
+      gedit # text editor
+      gnome-characters
+      gnome-music
+      gnome-photos
+      gnome-terminal
+      gnome-tour
+      hitori # sudoku game
+      iagno # go game
+      tali # poker game
+      totem # video player
+    ]);
   };
 
   users.users.nouseforaname = {
