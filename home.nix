9a36560597e0de4bb0a7f57e7e4a0ac152bcb23a
nouseforaname <34882943+nouseforaname@users.noreply.github.com>
2025-06-24 16:17:14 +0200

    rebuild nixos config in home manager
    most of the config is now moved to home-manager. unfortunately the
    new laptop is not allowed to run nixos.. This is a quick attempt at
    getting most of the functionality moved to home manager.. because why
    would home-manager and nixos use the same paths for configs :/

--- home.nix
+++ home.nix
@@ -1,40 +1,78 @@
-{ config, pkgs, ... }:
-
+{ config, pkgs, lib,... }:
+let
+  go = pkgs.go;
+  golint = pkgs.golint.override {
+    buildGoModule = pkgs.buildGoModule.override { go = pkgs.go; };
+  };
+  betteralign = pkgs.callPackage ./pkgs/betteralign { buildGoModule = pkgs.buildGoModule.override { go = pkgs.go; }; };
+  gopium = pkgs.callPackage ./pkgs/gopium { buildGoModule = pkgs.buildGoModule.override { go = pkgs.go; }; };
+in
 {
+
+
+  nixpkgs.config.allowUnfree= true;
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "kiessk";
   home.homeDirectory = "/Users/kiessk";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
   home.stateVersion = "25.05"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
+  #required so installed fonts are found
+  fonts={
+    fontconfig.enable = true;
+  };
   home.packages = with pkgs; [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # hello
+    #fonts
+    nerd-fonts.iosevka
+    nerd-fonts.iosevka-term
+    nerd-fonts.symbols-only
+    nerd-fonts.space-mono
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+    #comms
+    slack
+
+    #terminal tools
+    fzf
+    ripgrep
+    wget
+    bat
+    direnv
+
+    xournalpp
+
+    #go dev tools
+    go
+    golint
+    ginkgo
+    betteralign
+    gopium
+    gopls
+
 
   ];
+  programs.starship =
+    let
+      getPreset = name: (with builtins; removeAttrs (fromTOML (readFile "${pkgs.starship}/share/starship/presets/${name}.toml")) ["\"$schema\""]);
+    in {
+    enable = true;
+    settings =
+      lib.recursiveUpdate
+      (lib.mergeAttrsList [
+        (getPreset "nerd-font-symbols")
+        (getPreset "gruvbox-rainbow")
+      ])
+    {
+
+    };
+  };
   programs.bash = {
-    enable = true; 
-    bashrcExtra = ''
+    enable = true;
+    bashrcExtra = builtins.readFile ./bashrc + ''
+      export PATH=~/.nix-profile/bin:$PATH
       export GPG_TTY="$(tty)"
       export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
-    '';
+    '' ;
   };
   programs.git = {
     enable = true;
@@ -53,8 +91,31 @@
       };
     };
   };
-  programs.alacritty.enable = true;
-  programs.gpg = { 
+
+  programs.alacritty = {
+     enable = true;
+     settings = {
+       window = {
+         opacity = 0.9;
+         blur = false;
+       };
+       font = {
+         normal = {
+           family = "IosevkaTerm Nerd Font";
+	   style = "Regular";
+         };
+       };
+       selection = {
+         save_to_clipboard = true;
+       };
+       terminal = {
+         shell = {
+           program = "${pkgs.bash}/bin/bash";
+         };
+       };
+     };
+  };
+  programs.gpg = {
     enable = true;
     settings = {
       personal-cipher-preferences="AES256 AES192 AES";
@@ -95,41 +156,76 @@
       pinentry-program /Users/kiessk/.nix-profile/Applications/pinentry-mac.app/Contents/MacOS/pinentry-mac
       default-cache-ttl 60
       max-cache-ttl 120
+      enable-ssh-support
+      pinentry-program /Users/kiessk/.nix-profile/bin/pinentry-mac
     '';
   };
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+  programs.tmux = {
+    package = pkgs.tmux;
+    enable = true;
+    shell = "${pkgs.bash}/bin/bash";
+    terminal = "xterm-256color";
+    keyMode = "vi";
+    plugins = [ pkgs.tmuxPlugins.nord ];
+    baseIndex = 1;
+    extraConfig = ''
+      set -g mouse off
+      set-window-option -g xterm-keys on
+      set -g pane-active-border-style bg=default,fg=red
+    '';
   };
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    defaultEditor = true;
+    extraConfig = builtins.readFile ./vimrc;
+    extraPackages = with pkgs; [
+      #LSPs implementation
+      gopls
+      rust-analyzer
+      nil
+      nixfmt
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/kiessk/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    # EDITOR = "emacs";
+      #linters
+      golint
+      statix #nix-linter
+      shellcheck
+    ];
+    plugins = with pkgs.vimPlugins; [
+      # git
+      git-blame-nvim
+
+      # fonts
+      nvim-web-devicons
+
+      # navigation // search
+      nvim-tree-lua
+      fzf-vim
+
+      # vim nix file highlighting & support
+      vim-nix
+
+      # markdown
+      markdown-preview-nvim
+
+      # snippets
+      luasnip
+
+      # config helper
+      nvim-lspconfig
+
+      # auto shell check
+      vim-shellcheck
+
+      #completion plugin
+      nvim-cmp
+
+      #completion sources
+      cmp_luasnip
+      cmp-nvim-lsp
+    ];
+
   };
 
   # Let Home Manager install and manage itself.
